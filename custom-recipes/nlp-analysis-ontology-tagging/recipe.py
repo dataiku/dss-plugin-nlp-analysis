f5a02c35e00eb4a456ebea4b8e9d6386b8bbec35
Jane Bellaiche <77485161+JaneBellaiche@users.noreply.github.com>
2021-03-02 13:32:20 +0100

    Loading parameters with DkuConfig

--- custom-recipes/nlp-analysis-ontology-tagging/recipe.py
+++ custom-recipes/nlp-analysis-ontology-tagging/recipe.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
+
 from dataiku.customrecipe import *
 import dataiku
 import pandas as pd
 from dataiku import pandasutils as pdu
 from dataiku.customrecipe import get_recipe_config
-from tagger import Tagger
+from language_dict import SUPPORTED_LANGUAGES_SPACY
+from dku_config import DkuConfig
 
 #All plugin parameters name
-MANDATORY_PARAMS = ['text_column',
-                    'language_column',
-                    'tag_column',
-                    'case_sensitivity',
-                    'lemmatization',
-                    'unicode_normalization',
-                    'possible_outputs']
+MATCHING_PARAMS       = ['case_sensitivity',
+                         'lemmatization',
+                         'unicode_normalization'
+                        ]
 
-OPTIONAL_PARAMS  = ['keyword_column',
-                   'category_column']
-
-
-#raise an error when called
-class ParameterError(ValueError):
-    pass
-
-#get input datasets
 def get_input_datasets(role):
     return dataiku.Dataset(get_input_names_for_role(role)[0])
 
@@ -31,70 +21,168 @@ def get_output_datasets(role):
     return dataiku.Dataset(get_output_names_for_role(role)[0])
 
 def load_settings():
-    settings = {}
-
     
-    #get inputs 
-    settings['text_input']     = get_input_datasets('documents_to_tag').get_dataframe()
-    settings['ontology_input'] = get_input_datasets('ontology_of_tags').get_dataframe()
+    text_input        = get_input_datasets('Document dataset').get_dataframe()
+    ontology_input    = get_input_datasets('Ontology dataset').get_dataframe()
 
-    #get parameters
-    for par in MANDATORY_PARAMS:
-        try:
-            settings[par] = get_recipe_config()[par]
-        except:
-            raise ParameterError(
-                'Missing input column : {} '.format(par)
-            )       
+    config=get_recipe_config()
     
-    key_col = get_recipe_config().get('keyword_column')
-    settings['keyword_column'] = key_col if key_col else None
+    
+    input_text_cols   = text_input.columns
+    input_onto_cols   = ontology_input.columns
+    
+    dku_config = DkuConfig(
+        local_vars    = dataiku.Project().get_variables()['local'],
+        local_prefix  = "Ontology-tagging"
+    )
+
+    dku_config.add_param(
+        name          = "text_input",
+        value         = text_input,
+        required      = True
+    )
+
+    dku_config.add_param(
+        name          = "ontology_input",
+        value         = ontology_input,
+        required      = True
+    )
+    
+    for par in MATCHING_PARAMS:
+            dku_config.add_param(
+            name     = par,
+            value    = config[par],
+            required = True
+            )
      
-    cat_column = get_recipe_config().get('category_column')
-    settings['category_column'] = cat_column if len(cat_column)>0 else None 
+    text_col         = config.get("text_column")
+    dku_config.add_param(
+    name             = "text_column",
+    value            = text_col,
+    required         = True,
+    checks           = [
+        {
+            'type'   : 'exists',
+            'err_msg': 'Missing input column : {}.\n'.format("Text_column")
+        },
+        {   'type'   : 'custom',
+            'cond'   : text_col in input_text_cols,
+            'err_msg': 'Invalid input column : {}.\n'.format("Text column")
+        }])
+    
+    dku_config.add_param(
+    name             = "language",
+    value            = config.get('language'),
+    required         = True,
+    checks           = [
+        {
+            'type'   : 'exists',
+            'err_msg': 'You must select one of the languages.\n'
+                       'If your dataset contains several languages, you can use "Language column" and create a column in your Document dataset containing the languages of the documents.\n'
+        },
+        {
+            'type'   : 'in',
+            'op'     : list(SUPPORTED_LANGUAGES_SPACY.keys()) + ['language_column'],
+            'err_msg': 'You must select one of the languages.\n'
+                       'If your dataset contains several languages, you can use "Language column" and create a column in your Document dataset containing the languages of the documents.'
+        }])
+    
+    lang_col         = config.get('language_column')
+    dku_config.add_param(
+    name             = "language_column",
+    value            = lang_col if lang_col else None,
+    required         = (dku_config.language == 'language_column'),
+    checks           = [
+        {
+            'type'   : "custom",
+            'cond'   : lang_col != None,
+            'err_msg': 'Missing input column : {}.\n'.format("Language column")
+        },
         
-    return settings
-
-#check if the given columns come from the right input datasets
-def check_columns(settings):
+        {
+            'type'   : 'custom',
+            'cond'   : lang_col in input_text_cols,
+            'err_msg': 'Invalid input column : {}.\n'.format("Language column")
+        }])
     
-    input_text_cols   = settings['text_input'].columns
-    input_onto_cols   = settings['ontology_input'].columns
+    tag_col          = config.get("tag_column")
+    dku_config.add_param(
+    name             = "tag_column",
+    value            = tag_col,
+    required         = True,
+    checks           = [
+        {
+            'type'   : 'exists',
+            'err_msg': 'Missing input column : {}.\n'.format("Tag column")
+        },
+        {   'type'   : 'custom',
+            'cond'   : tag_col in input_onto_cols,
+            'err_msg': 'Invalid input column : {}.\n'.format("Tag column")
+        }])
     
-    input_text_params = [ 'text_column',
-                          'language_column']
-    input_onto_params = ['tag_column',
-                         'keyword_column',
-                         'category_column']
+    key_col          = config.get("keyword_column")
+    dku_config.add_param(
+    name             = "keyword_column",
+    value            = key_col,
+    required         = False,
+    checks           = [
+        {
+            'type'   : 'custom',
+            'cond'   : key_col in input_onto_cols or key_col==None,
+            'err_msg': 'Invalid input column : {}.\n'.format("Keyword column")
+        }])
     
-    for col in input_text_params:
-        if settings[col] not in input_text_cols:
-            raise ParameterError(
-                'Invalid input column : {}'.format(col)
-            )
-    for col in input_onto_params:
-        if settings[col] is not None and settings[col] not in input_onto_cols:
-            raise ParameterError(
-            'Invalid input column : {}'.format(col)
-            )
+    cat_col          = config.get("category_column")
+    dku_config.add_param(
+    name             = "category_column",
+    value            = cat_col,
+    required         = False,
+    checks           = [
+        {
+            'type'   :'custom',
+            'cond'   : cat_col in input_onto_cols or cat_col==None,
+            'err_msg': 'Invalid input column : {}.\n'.format("Category column")
+        }])
+    
+    output           = config.get("output_format")
+    dku_config.add_param(
+    name             = "output_format",
+    value            = output,
+    required         = dku_config.category_column != None,
+    checks           = [
+        {
+            'type'   : 'exists',
+            'err_msg': 'Missing parameter : {}'.format('Output format')
+        }])
+    
+    output           = config.get("output_format_with_categories")
+    dku_config.add_param(
+    name             = "output_format",
+    value            = output,
+    required         = dku_config.category_column != None,
+    checks           = [
+        {
+            'type': 'exists',
+            'err_msg': 'Missing parameter : {}'.format('Output format')
+        }])
+    
+    return dku_config
 
-#give settings to the Tagger and get the created output dataset   #TODO decomment when adding class Tagger in python-lib
-#def call_tagger(settings):
-#    tagging = Tagger(settings)
-#    df      = tagging.tagging_proceedure()
-#    return df
+#give settings to the Tagger and get the created output dataset   
+def call_tagger(settings):
+    tagging             = Tagger(settings)
+    df                  = tagging.tagging_proceedure()
+    return df
 
-            
 def process_params():
+    settings            = load_settings()
+    tagged_documents_df = pd.DataFrame()
+    #tagged_documents_df = call_tagger(settings)    TODO decomment when using Tagger
     
-    settings                = load_settings()
-    check_columns(settings)
-    tagged_documents_df     = pd.DataFrame()
-    #tagged_documents_df     = call_tagger(settings) #TODO decomment when adding class Tagger in python-lib 
     
-    #write output dataset
-    output_ds               = get_output_datasets('tagged_documents')
+    #write output
+    output_ds = get_output_datasets('Tagged documents')
     output_ds.write_with_schema(tagged_documents_df)
-
+    
 #run
 process_params()
