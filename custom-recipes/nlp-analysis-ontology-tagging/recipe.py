77654d8197176f5d734840ea34c99f36a02865eb
admin <admin>
2021-03-03 12:07:22 +0100

    Edited file 'custom-recipes/nlp-analysis-ontology-tagging/recipe.py'

--- custom-recipes/nlp-analysis-ontology-tagging/recipe.py
+++ custom-recipes/nlp-analysis-ontology-tagging/recipe.py
@@ -1,27 +1,24 @@
 # -*- coding: utf-8 -*-
 import dataiku
-import pandas as pd
-from dataiku.customrecipe import *
-from dataiku import pandasutils as pdu
-from dku_config import DkuConfig
+from dataiku.customrecipe import get_output_names_for_role
 from dku_plugin_config_loading import load_settings
+from tagger import Tagger
 
-#give settings to the Tagger and get the created output dataset   
+
+# give settings to the Tagger and get the created output dataset
 def call_tagger(settings):
-    tagging             = Tagger(settings)
-    df                  = tagging.tagging_proceedure()
+    tagging = Tagger(settings)
+    df = tagging.tagging_proceedure()
     return df
 
+
 def process_params():
-    settings            = load_settings()
-    tagged_documents_df = pd.DataFrame()
-    #tagged_documents_df = call_tagger(settings)    TODO decomment when using Tagger
-    
-    
-    #write output
+    settings = load_settings()
+    tagged_documents_df = call_tagger(settings)
     output_ds = get_output_names_for_role('Tagged documents')
     output_ds = dataiku.Dataset(output_ds[0])
     output_ds.write_with_schema(tagged_documents_df)
-    
-#run
+
+
+# run
 process_params()
