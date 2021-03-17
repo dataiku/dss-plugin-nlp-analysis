db339bda831603d12b9c0b71cb61735efe106de0
Alex Combessie <alex.combessie@gmail.com>
2021-03-17 12:14:26 +0100

    Create test_ontology_tagger.py

---
+++ tests/python/unit/test_ontology_tagger.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# This is a test file intended to be used with pytest
+# pytest automatically runs all the function starting with "test_"
+# see https://docs.pytest.org for more information
+
+from tagger import Tagger
+
+
+def test_tagger():
+    # TODO write a real test
+    tagger = Tagger()
+    assert True
