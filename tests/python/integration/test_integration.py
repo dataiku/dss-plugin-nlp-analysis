5233d1e6c7e05d7ec3b544543932077d78da6463
Jane Bellaiche <77485161+JaneBellaiche@users.noreply.github.com>
2021-04-15 15:22:49 +0200

    Update test_integration.py

---
+++ tests/python/integration/test_integration.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from dku_plugin_test_utils import dss_scenario
+
+PROJECT_KEY = "TESTONTOLOGYTAGGER"
+
+"""Each scenario tests the three output formats"""
+
+def add_test_scenario(user_dss_clients,scenario_id):
+    """run any scenario"""
+    dss_scenario.run(
+        user_dss_clients,
+        project_key=PROJECT_KEY,
+        scenario_id=scenario_id
+    )
+
+def test_monolingual_category(user_dss_clients):
+    """Plugin parameters: -language: en
+    - category_column specified"""
+    add_test_scenario(user_dss_clients,"MONOLINGUALCATEGORY")
+
+
+def test_monolingual_no_category(user_dss_clients):
+    """Plugin parameters:
+    -language: en
+    - category_column not specified"""
+    add_test_scenario(user_dss_clients,"MONOLINGUALNOCATEGORY")
+
+
+def test_multilingual_category(user_dss_clients):
+    """Plugin parameters:
+    -language: language column with the 58 supported languages in it
+    -category_column specified"""
+    add_test_scenario(user_dss_clients,"MULTILINGUALCATEGORY")
+
+
+def test_multilingual_no_category(user_dss_clients):
+    """Plugin parameters:
+    -language: language column with the 58 supported languages in it
+    -category_column not specified"""
+    add_test_scenario(user_dss_clients,"MULTILINGUALNOCATEGORY")
