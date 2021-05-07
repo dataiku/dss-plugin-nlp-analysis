a6bb7d0ec7bc0ce07824f4032fbbecce1245346b
admin <jane.bellaiche@dataiku.com>
2021-05-07 19:46:59 +0200

    linting

--- tests/python/unit/test_lemmatization.py
+++ tests/python/unit/test_lemmatization.py
@@ -4,9 +4,42 @@
 # see https://docs.pytest.org for more information
 from ontology_tagger import Tagger
 import pandas as pd
-import pytest 
+import pytest
 
-@pytest.mark.parametrize("keyword,language", [("worked","en"),("trabajó","es"),("jobbet","nb"),("travaillé","fr"),("hat funktioniert","de"),("работал","ru"), ("zadziałało","pl"),("treballat", "ca"),("pracoval","cs"),("arbejdede","da"),("radio","hr"),("dolgozott", "hu"),("bekerja","id"),("lavorato","it"),("geschafft","lb"),("dirbo","lt"),("trabalhado","pt"),("a lucrat","ro"),("радио","sr"),("nagtrabaho","tl"),("çalıştı","tr"),("کام کیا","ur"),("কাজ করছে","bn"),("δούλεψε","el"),("کار کرد","fa"),("работел","mk"),("werkte","nl"),("arbetade","sv")])
+
+@pytest.mark.parametrize(
+    "keyword,language",
+    [
+        ("worked", "en"),
+        ("trabajó", "es"),
+        ("jobbet", "nb"),
+        ("travaillé", "fr"),
+        ("hat funktioniert", "de"),
+        ("работал", "ru"),
+        ("zadziałało", "pl"),
+        ("treballat", "ca"),
+        ("pracoval", "cs"),
+        ("arbejdede", "da"),
+        ("radio", "hr"),
+        ("dolgozott", "hu"),
+        ("bekerja", "id"),
+        ("lavorato", "it"),
+        ("geschafft", "lb"),
+        ("dirbo", "lt"),
+        ("trabalhado", "pt"),
+        ("a lucrat", "ro"),
+        ("радио", "sr"),
+        ("nagtrabaho", "tl"),
+        ("çalıştı", "tr"),
+        ("کام کیا", "ur"),
+        ("কাজ করছে", "bn"),
+        ("δούλεψε", "el"),
+        ("کار کرد", "fa"),
+        ("работел", "mk"),
+        ("werkte", "nl"),
+        ("arbetade", "sv"),
+    ],
+)
 def test_lemmatize_keywords(keyword, language):
     tagger = Tagger(
         ontology_df=pd.DataFrame({"tag": ["verb"], "keyword": [keyword]}),
