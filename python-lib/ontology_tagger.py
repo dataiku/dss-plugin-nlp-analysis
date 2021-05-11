601267b85372709c150002d2d51722bc15339ff7
admin <jane.bellaiche@dataiku.com>
2021-05-11 23:05:50 +0200

    fix import

--- python-lib/ontology_tagger.py
+++ python-lib/ontology_tagger.py
@@ -11,8 +11,8 @@ from time import perf_counter
 import logging
 from sentence_splitter import SentenceSplitter
 from language_support import SPACY_LANGUAGE_LOOKUP
-from language_support SPACY_LANGUAGE_RULES
-from language_support SPACY_LANGUAGE_MODELS_LEMMATIZATION
+from language_support import SPACY_LANGUAGE_RULES
+from language_support import SPACY_LANGUAGE_MODELS_LEMMATIZATION
 
 
 class Tagger:
