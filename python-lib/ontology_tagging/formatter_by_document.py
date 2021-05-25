73820b5c4506c984f3b4caa989cff7b3922d750b
admin <jane.bellaiche@dataiku.com>
2021-05-25 20:42:27 +0200

    rewrite get_tags_in_row

--- python-lib/ontology_tagging/formatter_by_document.py
+++ python-lib/ontology_tagging/formatter_by_document.py
@@ -27,9 +27,10 @@ class FormatterByDocument(FormatterBase):
         super().__init__(*args, **kwargs)
         self.tag_sentences, self.tag_keywords = [], []
 
-    def _fill_tags(self, condition: bool, value: dict):
-        """Dump the dictionary 'value' if 'condition' is True"""
-        return json.dumps(value, ensure_ascii=False) if condition else np.nan
+    @staticmethod
+    def _fill_tags(value: dict, not_empty: bool = True):
+        """Dump the dictionary 'value' if 'not_empty' is True"""
+        return json.dumps(value, ensure_ascii=False) if not_empty else np.nan
 
     def write_df(
         self,
@@ -64,69 +65,60 @@ class FormatterByDocument(FormatterBase):
         """
         language = self._get_document_language(row, language_column)
         document_to_match = self._get_document_to_match(row, language)
-        tags_in_document, keywords_in_document, matched_sentences = [], [], []
+        tags_in_document = []
+        keywords_in_document = []
+        matched_sentences = []
         for idx, sentence in enumerate(document_to_match):
-            (
-                tags_in_document,
-                keywords_in_document,
-                matched_sentences,
-            ) = self._get_tags_in_row(
+            original_sentence = row[self.text_column_tokenized][idx]
+            tags_in_sentence, keywords_in_sentence = self._get_tags_in_sentence(
                 sentence=sentence,
-                original_sentence=row[self.text_column_tokenized][idx],
-                tags_in_document=tags_in_document,
-                keywords_in_document=keywords_in_document,
-                matched_sentences=matched_sentences,
+                original_sentence=original_sentence,
                 language=language,
             )
+            tags_in_document.extend(tags_in_sentence)
+            keywords_in_document.extend(keywords_in_sentence)
+            if tags_in_sentence:
+                matched_sentences.append(original_sentence + "\n")
 
-        if tags_in_document != []:
-            line = {
-                self.tag_columns[0]: self._fill_tags(
-                    True, unique_list(tags_in_document)
-                ),
-                self.tag_columns[1]: self._fill_tags(
-                    True, unique_list(keywords_in_document)
-                ),
-                self.tag_columns[2]: "".join(unique_list(matched_sentences)),
+        if tags_in_document:
+            tag_datas = {
+                self.tag_columns[0]: self._fill_tags(unique_list(tags_in_document)),
+                self.tag_columns[1]: self._fill_tags(unique_list(keywords_in_document)),
+                self.tag_columns[2]: "".join(matched_sentences),
             }
         else:
-            line = {column: np.nan for column in self.tag_columns}
-        self.output_df = self.output_df.append(line, ignore_index=True)
+            tag_datas = {column: np.nan for column in self.tag_columns}
+        self.output_df = self.output_df.append(tag_datas, ignore_index=True)
 
-    def _get_tags_in_row(
+    def _get_tags_in_sentence(
         self,
         sentence: Doc,
         original_sentence: Span,
-        tags_in_document: List,
-        keywords_in_document: List,
-        matched_sentences: List,
         language: AnyStr,
-    ) -> Tuple[List[AnyStr], List[AnyStr], List[AnyStr]]:
+    ) -> Tuple[List[AnyStr], List[AnyStr]]:
         """
-        Called by _write_row on each sentence
+        Called by _write_row on each sentence.
 
         Args:
             sentence (Doc): the sentence to search matches in (possibly previously normalized by _get_document_to_match)
             original_sentence (Span): the original sentence from the input dataframe
-            tags_in_document (List): List of tags found in the current document
-            keywords_in_document (List): List of keywords tags found in the current document
-            matched_sentences (List): List of matched sentences in the current document
             language (AnyStr): Language of the current document
 
         Returns:
-            Lists of tags, keywords and sentences enriched with 'sentence' matches
+            Lists of tags and keywords found in a sentence
 
         """
         matches = self._matcher_dict[language](sentence, as_spans=True)
+        tags_in_sentence = []
+        keywords_in_sentence = []
         for match in matches:
             keyword = match.text
             tag = self._keyword_to_tag[language][
                 get_span_text(span=match, lemmatize=self.lemmatization)
             ]
-            tags_in_document.append(tag)
-            keywords_in_document.append(keyword)
-            matched_sentences.append(original_sentence + "\n")
-        return tags_in_document, keywords_in_document, matched_sentences
+            tags_in_sentence.append(tag)
+            keywords_in_sentence.append(keyword)
+        return tags_in_sentence, keywords_in_sentence
 
     def write_df_category(
         self,
@@ -177,85 +169,33 @@ class FormatterByDocument(FormatterBase):
         """
         language = self._get_document_language(row, language_column)
         document_to_match = self._get_document_to_match(row, language)
-        matched_sentence, keyword_list = [], []
-        tag_columns_for_json, line, line_full = (
-            defaultdict(),
-            defaultdict(list),
-            defaultdict(defaultdict),
-        )
+        matched_sentence = []
+        keyword_list = []
+        categories_and_tags = defaultdict(list)
         for idx, sentence in enumerate(document_to_match):
-            original_sentence = row[self.text_column_tokenized]
+            original_sentence = row[self.text_column_tokenized][idx]
             for keyword in sentence.ents:
-                line, line_full = self._get_tags_in_row_category(
-                    match=keyword,
-                    line=line,
-                    line_full=line_full,
-                    sentence=original_sentence[idx],
-                    language=language,
-                )
-                keyword_list.append(keyword.text + " ")
-                matched_sentence.append(original_sentence[idx] + "\n")
-            tag_columns_for_json["tag_json_categories"] = self._fill_tags(
-                condition=(line and one_row_per_doc_json), value=dict(line)
-            )
-            tag_columns_for_json["tag_json_full"] = self._fill_tags(
-                condition=(line and one_row_per_doc_json),
-                value={
-                    column_name: dict(value) for column_name, value in line_full.items()
-                },
-            )
-        line = {column: self._fill_tags(True, line[column]) for column in line}
+                tag = keyword.ent_id_
+                category = keyword.label_
+                if tag not in categories_and_tags[category]:
+                    categories_and_tags[category].append(tag)
+                if keyword.text not in keyword_list:
+                    keyword_list.append(keyword.text)
+                if original_sentence + "\n" not in matched_sentence:
+                    matched_sentence.append(original_sentence + "\n")
+        categories_and_tags = {
+            column: self._fill_tags(categories_and_tags[column])
+            for column in categories_and_tags
+        }
         if keyword_list:
-            self.tag_keywords.append(self._fill_tags(True, unique_list(keyword_list)))
+            self.tag_keywords.append(self._fill_tags(keyword_list))
         else:
             self.tag_keywords.append(np.nan)
-        self.tag_sentences.append(" ".join(unique_list(matched_sentence)))
-        self.output_df = (
-            self.output_df.append(tag_columns_for_json, ignore_index=True)
-            if one_row_per_doc_json
-            else self.output_df.append(
-                line,
-                ignore_index=True,
-            )
+        self.tag_sentences.append(" ".join(matched_sentence))
+        self.output_df = self.output_df.append(
+            categories_and_tags,
+            ignore_index=True,
         )
-
-    def _get_tags_in_row_category(
-        self, match: Span, line: dict, line_full: dict, sentence: Doc, language: AnyStr
-    ) -> Tuple[dict, dict]:
-        """
-        Called by _write_row_category
-        Enrich 'line' and 'line_full' dictionaries with new found tags
-
-        Args:
-            line (dict): dictionary of category (keys) and tags (values)
-            line_full (dict): dictionary of category (keys).
-                Each category has for value a dictionary of tags and their associated datas (keywords, sentences, occurences of the tag)
-            sentence (Doc): sentence that matched with keyword(s)
-            language (AnyStr): language of the sentence
-
-        Returns:
-            - Tuple(dict,dict) : The enriched dictionaries 'line' and 'line_full'
-
-        """
-        keyword = match.text
-        tag = match.ent_id_
-        category = match.label_
-        sentence = sentence
-        if tag not in line_full[category]:
-            line_full[category][tag] = {
-                "count": 1,
-                "sentences": [sentence],
-                "keywords": [keyword],
-            }
-
-            line[category].append(tag)
-        else:
-            line_full[category][tag]["count"] += 1
-            if sentence not in line_full[category][tag]["sentences"]:
-                line_full[category][tag]["sentences"].append(sentence)
-            if keyword not in line_full[category][tag]["keywords"]:
-                line_full[category][tag]["keywords"].append(keyword)
-        return line, line_full
 
     def _merge_df_columns_category(
         self, input_df: pd.DataFrame, text_column: AnyStr
@@ -355,25 +295,33 @@ class FormatterByDocumentJson(FormatterByDocument):
         """
         language = self._get_document_language(row, language_column)
         document_to_match = self._get_document_to_match(row, language)
-        line_full, tag_column_for_json = defaultdict(defaultdict), {}
+        tags_full, tag_column = defaultdict(defaultdict), {}
         for idx, sentence in enumerate(document_to_match):
             matches = self._matcher_dict[language](sentence, as_spans=True)
             for keyword in matches:
-                line_full = self._get_tags_in_row(
-                    match=keyword,
-                    line_full=line_full,
-                    original_sentence=row[self.text_column_tokenized][idx],
-                    language=language,
-                )
-
-        tag_column_for_json["tag_json_full"] = self._fill_tags(
-            condition=line_full,
+                original_sentence = row[self.text_column_tokenized][idx]
+                tag = self._keyword_to_tag[language][
+                    get_span_text(span=keyword, lemmatize=self.lemmatization)
+                ]
+                if tag not in tags_full.keys():
+                    tags_full[tag] = {
+                        "count": 1,
+                        "sentences": [original_sentence],
+                        "keywords": [keyword.text],
+                    }
+                else:
+                    tags_full[tag]["count"] += 1
+                    if original_sentence not in tags_full[tag]["sentences"]:
+                        tags_full[tag]["sentences"].append(original_sentence)
+                    if keyword.text not in tags_full[tag]["keywords"]:
+                        tags_full[tag]["keywords"].append(keyword.text)
+        tag_column["tag_json_full"] = self._fill_tags(
+            not_empty=bool(tags_full),
             value={
-                column_name: dict(value) for column_name, value in line_full.items()
+                column_name: dict(value) for column_name, value in tags_full.items()
             },
         )
-
-        self.output_df = self.output_df.append(tag_column_for_json, ignore_index=True)
+        self.output_df = self.output_df.append(tag_column, ignore_index=True)
 
     def write_df_category(
         self,
@@ -403,36 +351,51 @@ class FormatterByDocumentJson(FormatterByDocument):
         )
         return self._set_columns_order(input_df, self.output_df, text_column)
 
-    def _get_tags_in_row(
-        self,
-        match: Span,
-        line_full: dict,
-        original_sentence: Doc,
-        language: AnyStr,
-    ) -> dict:
-        """
-        Called by _write_row on each sentence
-        Returns:
-            dict:  Dictionary of tags(keys). Each tag has for value a dictionary defined by the following keys:
-                -key 'count' : number of occurences of the the keywords in the document
-                -key 'sentences' : list of the sentences that matched keywords in the document
-                -key 'keywords' : list of the keywords in the document with details about each tags
-        """
-        keyword = match.text
-        tag = self._keyword_to_tag[language][
-            get_span_text(span=match, lemmatize=self.lemmatization)
-        ]
-        if tag not in line_full.keys():
-            line_full[tag] = {
-                "count": 1,
-                "sentences": [original_sentence],
-                "keywords": [keyword],
-            }
-        else:
-            line_full[tag]["count"] += 1
-            if original_sentence not in line_full[tag]["sentences"]:
-                line_full[tag]["sentences"].append(original_sentence)
-            if keyword not in line_full[tag]["keywords"]:
-                line_full[tag]["keywords"].append(keyword)
+    def _write_row_category(
+        self, row: pd.Series, one_row_per_doc_json: bool, language_column: AnyStr = None
+    ) -> None:
+        language = self._get_document_language(row, language_column)
+        document_to_match = self._get_document_to_match(row, language)
+        tag_columns = defaultdict()
+        categories_and_tags = defaultdict(list)
+        categories_and_tags_full = defaultdict(defaultdict)
+        for idx, sentence in enumerate(document_to_match):
+            original_sentence = row[self.text_column_tokenized]
+            for keyword in sentence.ents:
+                tag = keyword.ent_id_
+                category = keyword.label_
+                if tag not in categories_and_tags_full[category]:
+                    categories_and_tags_full[category][tag] = {
+                        "count": 1,
+                        "sentences": [original_sentence[idx]],
+                        "keywords": [keyword.text],
+                    }
+                    categories_and_tags[category].append(tag)
+                else:
+                    categories_and_tags_full[category][tag]["count"] += 1
+                    if (
+                        original_sentence[idx]
+                        not in categories_and_tags_full[category][tag]["sentences"]
+                    ):
+                        categories_and_tags_full[category][tag]["sentences"].append(
+                            original_sentence[idx]
+                        )
+                    if (
+                        keyword.text
+                        not in categories_and_tags_full[category][tag]["keywords"]
+                    ):
+                        categories_and_tags_full[category][tag]["keywords"].append(
+                            keyword.text
+                        )
 
-        return line_full
+            tag_columns["tag_json_categories"] = self._fill_tags(
+                not_empty=bool(categories_and_tags), value=dict(categories_and_tags)
+            )
+            tag_columns["tag_json_full"] = self._fill_tags(
+                not_empty=bool(categories_and_tags),
+                value={
+                    column_name: dict(value)
+                    for column_name, value in categories_and_tags_full.items()
+                },
+            )
+        self.output_df = self.output_df.append(tag_columns, ignore_index=True)
